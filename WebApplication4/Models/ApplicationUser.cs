5b9e0b4ced9eefff35d77fe444f99e7c4202993c
Administrator <Administrator@PC229>
2020-06-01 11:25:52 +0700

    NGAy1-LAb4-5-6

--- WebApplication4/Models/ApplicationUser.cs
+++ WebApplication4/Models/ApplicationUser.cs
@@ -15,6 +15,7 @@ namespace WebApplication4.Models
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
             return userIdentity;
+
         }
     }
 
